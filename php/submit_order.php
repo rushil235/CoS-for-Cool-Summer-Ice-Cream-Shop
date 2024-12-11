536ef618ad4eb4e5d7869a8050bb4e0645741507
Rejoice0420 <r.itur985@mybvc.ca>
2024-12-11 12:09:38 -0700

    Update submit_order.php

--- php/submit_order.php
+++ php/submit_order.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
-include 'database.php';
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Replace with your RDS database credentials
+$servername = "csicg4.czptxhzjxjrt.us-east-1.rds.amazonaws.com";
+$username = "group4";
+$password = "Groupfour";
+$dbname = "ice_shop";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $conn = getDatabaseConnection();
 
