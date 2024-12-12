7f2138bb6746593bc54d2f2e0f454dad46511112
Rejoice0420 <r.itur985@mybvc.ca>
2024-12-11 17:19:35 -0700

    Update adminlogin.php

--- php/adminlogin.php
+++ php/adminlogin.php
@@ -42,7 +42,7 @@
         <?php endif; ?>
         <form method="POST">
             <label for="username">Username</label> 
-            <input type="text" id="username" name="username" required>
+            <input type="text" id="username" name="username" required><br>
 
             <label for="password">Password</label>
             <input type="password" id="password" name="password" required>
